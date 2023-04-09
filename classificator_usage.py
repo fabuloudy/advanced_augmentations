e7539e29cee58b4071cb9f77e104f254d3bf6c2a
Romanovskaya <romanovskaya.ym@gmail.com>
2023-04-09 21:28:20 +0300

    Refactored version

--- classificator_usage.py
+++ classificator_usage.py
@@ -1,43 +1,11 @@
-from pysistant import helpers
-import torch
 import torchvision.models as models
-from torch.nn import Module
-from torch.nn.modules import loss
-from torch import optim
-from tqdm import tqdm
-from IPython.display import Audio
-import numpy as np
-from sklearn.metrics import ConfusionMatrixDisplay, confusion_matrix
-import matplotlib.pyplot as plt
-from sklearn.metrics import classification_report
-from sklearn.metrics import roc_auc_score, average_precision_score, roc_curve
-import pickle
-import numpy as np
 import torch
-import torchaudio as T
-import torchaudio.transforms as TT
-from sklearn import preprocessing
-from augmentation_utils import get_batch
-
-from argparse import ArgumentParser
-from concurrent.futures import ProcessPoolExecutor
-from glob import glob
-from tqdm import tqdm
-import numpy as np
-from sklearn.metrics import ConfusionMatrixDisplay, confusion_matrix
-import matplotlib.pyplot as plt
-import os.path
 from metrics import count_metrics
 
-from dataset_creation import SoundDS, split_train_val_test, load_from_file
-from models_usage import MelGanTool
-
-DEVICE = "cuda:0" if torch.cuda.is_available() else "cpu"
 BATCH_SIZE = 64
 CLASS_TO_ID = {}
 
 device = "cuda:0" if torch.cuda.is_available() else "cpu"
-
 
 def train(dataloader, model, loss_fn, optimizer):
     size = len(dataloader.dataset)
@@ -80,10 +48,6 @@ def validation(dataloader, model, loss_fn):
     correct /= size
     print(f"Val Error: \n Accuracy: {(100 * correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
 
-
-from sklearn.metrics import classification_report
-
-
 def test(dataloader, model):
     size = len(dataloader.dataset)
     num_batches = len(dataloader)
@@ -100,15 +64,11 @@ def test(dataloader, model):
             correct += (pred.argmax(1) == y).type(torch.float).sum().item()
 
     correct /= size
-    print('ALLO')
     print(f"Test Error: \n Accuracy: {(100 * correct):>0.1f}\n")
     return y_true, y_pred
 
-
-
-
 def run_resnet(train_dataloader, val_dataloader, test_dataloader, matrix_filename, weights_file_name = None):
-    model = models.resnet18().to(DEVICE)
+    model = models.resnet18().to(device)
     loss_fn = torch.nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
 
@@ -117,13 +77,8 @@ def run_resnet(train_dataloader, val_dataloader, test_dataloader, matrix_filenam
         print(f"Epoch {t + 1}\n-------------------------------")
         train(train_dataloader, model, loss_fn, optimizer)
         validation(val_dataloader, model, loss_fn)
-        test(test_dataloader, model)
-
     print("Done!")
 
-    # model.eval()
     y_true, y_pred = test(test_dataloader, model)
     count_metrics(y_true, y_pred, matrix_filename)
-
-
     print("Done!")
